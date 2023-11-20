4e14ba9d7315bedebba890bceac2ff6bda0ff0bf
wolterbwambale <benplaan1@gmail.com>
2023-11-20 15:32:06 +0300

    add validation to cotegory model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :user
+
+  has_many :transaction_entities, dependent: :destroy
+  has_many :transactions, through: :transaction_entities, dependent: :destroy
+
+  validates :name, presence: true
+  validates :icon, presence: true
 end

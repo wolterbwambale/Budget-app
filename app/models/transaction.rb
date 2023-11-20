58cd2a8b75e45f48d458a79d77dfdb4ef42a13b3
wolterbwambale <benplaan1@gmail.com>
2023-11-20 15:34:13 +0300

    add transaction validation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :author
+  has_many :transaction_entities, dependent: :destroy
+  has_many :categories, through: :transaction_entities, dependent: :destroy
+
+  validates :name, presence: true
+  validates :amount, presence: true
 end

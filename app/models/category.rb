a9c72eb575e4ff8e4c20f9b7e0bb9db989021b1e
wolterbwambale <benplaan1@gmail.com>
2023-11-22 17:17:10 +0300

    modify the category file

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,4 @@
 class Category < ApplicationRecord
-  belongs_to :user
-
-  has_many :transaction_entities, dependent: :destroy
-  has_many :transactions, through: :transaction_entities, dependent: :destroy
-
-  validates :name, presence: true
-  validates :icon, presence: true
+  has_many :transactions, dependent: :destroy
+  attribute :total_amount, :integer
 end

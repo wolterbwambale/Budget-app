d36046f2080ecfd344b69c96205e4e00221ffed8
wolterbwambale <benplaan1@gmail.com>
2023-11-22 17:15:02 +0300

    add ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,7 @@ class Ability
 
   def initialize(user)
     can :read, Category, user_id: user.id
-    can :read, Transaction, user_id: user.id
+    can :read,:create, Transaction, user_id: user.id
     can :Detroy, Category, user_id: user.id
     can :manage, Category, user_id: user.id
   end

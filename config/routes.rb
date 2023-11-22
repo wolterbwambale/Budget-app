69519cdc2d879d848d90a590b9ec826f427dd62b
wolterbwambale <benplaan1@gmail.com>
2023-11-22 13:33:44 +0300

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  root 'landing_page#splash'
+
   devise_for :users
+  resources :categories, only: [:index, :show, :new, :create, :destroy] do
+    resources :transactions, only: [:index, :show, :new, :create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
+

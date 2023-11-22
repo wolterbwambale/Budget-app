17742cf115d9555ed63b8ecd66072d8a559d283e
wolterbwambale <benplaan1@gmail.com>
2023-11-22 17:17:25 +0300

    modify the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable,  and :omniauthable
-  devise :database_authenticatable, :registerable,:confirmable, :trackable,
+  devise :database_authenticatable, :registerable, :confirmable, :trackable,
          :recoverable, :rememberable, :validatable
 
-         has_many :categories
-         has_many :transactions
-
-         validates:name, presence: true
+  has_many :categories
+  has_many :transactions
 end

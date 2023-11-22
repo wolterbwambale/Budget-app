7f167838a306377dda661a9df54a8d92fca21f98
wolterbwambale <benplaan1@gmail.com>
2023-11-22 17:26:04 +0300

    add transaction spec test

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+    before :each do
+      @transaction = Transaction.new(name: 'Clothes', amount: 50)
+    end
+  
+    it 'name should be present' do
+      expect(@transaction.name).to eq('Clothes')
+    end
+  
+    it 'amount should be present' do
+      expect(@transaction.amount).to eq(50)
+    end
+  end
+  

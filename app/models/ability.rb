0963d4df3f7fc9c2ace0e52ebaf3582a0a4f79c5
wolterbwambale <benplaan1@gmail.com>
2023-11-22 13:32:16 +0300

    add cancan ability

---
+++ app/models/ability.rb
@@ -0,0 +1,10 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, Category, user_id: user.id
+    can :read, Transaction, user_id: user.id
+    can :Detroy, Category, user_id: user.id
+    can :manage, Category, user_id: user.id
+  end
+end
